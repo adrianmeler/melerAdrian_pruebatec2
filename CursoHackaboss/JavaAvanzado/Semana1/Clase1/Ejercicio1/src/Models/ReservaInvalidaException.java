2688d5d5dfbdccf82d741c30ebd3e737fba306e3
Adrian <adrian.meler@hotmail.com>
2024-01-01 11:44:48 +0100

    commit

---
+++ CursoHackaboss/JavaAvanzado/Semana1/Clase1/Ejercicio1/src/Models/ReservaInvalidaException.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Models;
+
+/**
+ *
+ * @author Adrian
+ */
+public class ReservaInvalidaException extends Exception{
+
+    public ReservaInvalidaException() {
+    }
+
+    public ReservaInvalidaException(String message) {
+        super(message);
+    }
+    
+}
